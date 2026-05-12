54150f282d0699a92b3e301a0171442f022651df
Odin Acceptance <odin@example.com>
2026-05-12 14:03:43 +0000

    fix: load operator env for readiness checks

---
+++ tests/integration/testmain_test.go
@@ -0,0 +1,25 @@
+package integration_test
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	envDir, err := os.MkdirTemp("", "odin-integration-env-*")
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "create integration env dir: %v\n", err)
+		os.Exit(1)
+	}
+	if err := os.Setenv("ODIN_ENV_FILE", filepath.Join(envDir, "missing-odin-os.env")); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "set ODIN_ENV_FILE: %v\n", err)
+		_ = os.RemoveAll(envDir)
+		os.Exit(1)
+	}
+
+	code := m.Run()
+	_ = os.RemoveAll(envDir)
+	os.Exit(code)
+}
